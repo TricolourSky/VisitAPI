b7182f871471b11ae05cd0e503674173bee1e6ac
Tricolourの空 <lebem1772903718@gmail.com>
2026-08-30 06:15:08 +1000

    Delete Client directory

--- Client/Native/DialogFiles.cs
+++
@@ -1,9 +0,0 @@
-using System.IO;
-using VisitAPI.Dialog;
-
-namespace VisitAPI.Native;
-
-public static class DialogFiles
-{
-    public static readonly DialogLoader Loader = new(Path.Combine(BepInEx.Paths.ConfigPath, "VisitAPI"));
-}
